759f09c562bcaf8c57e80f1a7c3b4f1afba6c938
Jewel James <flakyhermit@protonmail.ch>
2022-03-23 11:31:42 +0530

    fake db

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Find a bookkeeper API")
 
-class Bookkeeper():
-    id: int
-    name: str
-    bio: str
-    def __init__(self, id, name, bio):
-        self.id = id
-        self.name = name
-        self.bio = bio
-
 @app.get("/")
 async def read_root():
     return { "title": "Find a Bookkeeper", "message": "Welcome to find a bookkeeper API. Check the docs for info." }
@@ -22,13 +13,28 @@ async def read_bookkeepers(skip: int = 0, limit: int = 20):
     # Return only limit number of parameters
     return { "name": "Jewel James" }
 
-data = {
-    "id": 1,
-    "name": "Martin Thomas",
-    "bio": "The founder of Weekend Bookkeeper, himself a weekend bookkeeper."
-}
+DATA = [
+    {
+        "id": 1,
+        "name": "Martin Thomas",
+        "bio": "The founder of Weekend Bookkeeper, himself a weekend bookkeeper."
+    },
+    {
+        "id": 2,
+        "name": "Jewel James",
+        "bio": "The lead developer of Weekend Bookkeeper, and its chief economic adviser."
+    },
+    {
+        "id": 3,
+        "name": "Breezy",
+        "bio": "The Weekend Bookkeeper pet robot."
+    }
+]
 
-bookkeeper = Bookkeeper(**data)
 @app.get("/bookkeepers/{bookkeeper_id}")
 async def read_bookkeeper(bookkeeper_id: int):
-    return bookkeeper
+    result = [bookkeeper for bookkeeper in DATA if bookkeeper["id"] == bookkeeper_id]
+    if result:
+        return result[0]
+    else:
+        return { "message": "id not found in db" }
