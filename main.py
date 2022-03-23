b6649b35a77ba28bbc5b0254417a4217988f93d8
Jewel James <flakyhermit@protonmail.ch>
2022-03-23 11:06:18 +0530

    async + class for data validation

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class Bookkeeper():
+    id: int
+    name: str
+    bio: str
+    def __init__(self, id, name, bio):
+        self.id = id
+        self.name = name
+        self.bio = bio
+
 @app.get("/")
-def read_root():
+async def read_root():
     return { "title": "Find a Bookkeeper", "message": "Welcome to find a bookkeeper API. Check the docs for info." }
 
 @app.get("/bookkeepers/")
-def read_bookkeepers(skip: int = 0, limit: int = 20):
+async def read_bookkeepers(skip: int = 0, limit: int = 20):
     # Return only limit number of parameters
     return { "name": "Jewel James" }
 
+data = {
+    "id": 1,
+    "name": "Martin Thomas",
+    "bio": "The founder of Weekend Bookkeeper, himself a weekend bookkeeper."
+}
+
+bookkeeper = Bookkeeper(**data)
 @app.get("/bookkeepers/{bookkeeper_id}")
-def read_bookkeeper(bookkeeper_id: int):
-    return { "id": bookkeeper_id, "name": "Dummy Name" }
+async def read_bookkeeper(bookkeeper_id: int):
+    return bookkeeper
