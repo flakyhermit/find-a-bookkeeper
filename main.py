8492573c361b6b9be6c9f3ba5eceb0aea0f5a774
Jewel James <flakyhermit@protonmail.ch>
2022-03-24 10:39:26 +0530

    response model added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Find a bookkeeper API")
 
 @app.get("/")
 async def read_root():
     return { "title": "Find a Bookkeeper API", "message": "Welcome to find a bookkeeper API. Check the docs for info." }
+
+class Bookkeeper(BaseModel):
+    """ The model for a bookkeeper """
+    id: int
+    name: str
+    bio: str
 
 DATA = [
     {
@@ -37,7 +44,7 @@ async def search_bookkeepers(keyword: str | None = None, limit: int = 20) -> lis
         return result[:limit]
     return DATA[:limit]
 
-@app.get("/bookkeepers/{bookkeeper_id}")
+@app.get("/bookkeepers/{bookkeeper_id}", response_model = Bookkeeper)
 async def read_bookkeeper(bookkeeper_id: int) -> dict:
     result = [bookkeeper for bookkeeper in DATA if bookkeeper["id"] == bookkeeper_id]
     if result:
