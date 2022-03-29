027f92f168603c8104733ae74959321ee2da9479
Jewel James <flakyhermit@protonmail.ch>
2022-03-29 14:23:41 +0530

    fix: schema import

--- crud.py
+++ crud.py
@@ -3,6 +3,7 @@
 from sqlalchemy.orm import Session
 
 import models
+import schema
 
 def get_bookkeepers(db: Session, skip: int, limit: int):
     return db.query(models.Bookkeeper).offset(skip).limit(limit).all()
