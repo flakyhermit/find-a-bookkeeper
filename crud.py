b58ab0c307651693b051799509c500ec09355ab9
Jewel James <flakyhermit@protonmail.ch>
2022-03-29 17:05:32 +0530

    deleted old functions

--- crud.py
+++ crud.py
@@ -56,39 +56,3 @@ class CRUDBookkeepers(CRUDBase[models.Bookkeeper, schemas.BookkeeperCreate]):
         return result
 
 bookkeeper = CRUDBookkeepers(models.Bookkeeper, schemas.BookkeeperCreate)
-
-def get_bookkeepers(db: Session, skip: int, limit: int):
-    return db.query(models.Bookkeeper).offset(skip).limit(limit).all()
-
-def get_bookkeeper_by_id(db: Session, id: int):
-    result = db.query(models.Bookkeeper).filter(models.Bookkeeper.id == id).first()
-    return result
-
-def search_bookkeepers_by_name(db: Session, search: str, skip: int, limit: int):
-    result = db.query(models.Bookkeeper).filter(
-        models.Bookkeeper.name.like(f'%{search}%')
-    ).offset(skip).limit(limit).all()
-    return result
-
-def create_bookkeeper(db: Session, item: schemas.BookkeeperCreate):
-    bookkeeper = models.Bookkeeper(name = item.name, bio = item.bio)
-    db.add(bookkeeper)
-    db.commit()
-    db.refresh(bookkeeper)
-    return bookkeeper
-
-def delete_bookkeeper(db: Session, id: int):
-    bookkeeper = db.query(models.Bookkeeper).get(id)
-    if bookkeeper is not None:
-        db.delete(bookkeeper)
-        db.commit()
-    return bookkeeper
-
-def update_bookkeeper(db: Session, id: int, item: schemas.BookkeeperCreate):
-    bookkeeper = db.query(models.Bookkeeper).get(id)
-    if bookkeeper is not None:
-        bookkeeper.name = item.name
-        bookkeeper.bio = item.bio
-        db.commit()
-        db.refresh(bookkeeper)
-    return bookkeeper
