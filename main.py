8cbe5ca7752d9bef4f5b90d7a5f250f907757362
Jewel James <flakyhermit@protonmail.ch>
2022-03-30 11:11:38 +0530

    removed comments

--- main.py
+++ main.py
@@ -97,33 +97,5 @@ async def delete_service(service_id: int):
         status_code = 404,
         detail = f"There's no item with id: {service_id}"
     )
-# # Services
-# @app.get("/services", response_model = ServicesSearchResult)
-# async def read_services(skip: int = 0, limit: int = 20) -> list[dict]:
-#     d = db.read_services()
-#     return { "results" : d[skip: skip + limit] }
-
-# @app.get("/services/{service_id}", response_model = Service)
-# async def read_service(service_id: int) -> dict[Service]:
-#     result = db.read_service(service_id)
-#     if result:
-#         return result
-#     raise HTTPException(
-#         status_code = 404,
-#         detail = f"There's no item with id: {service_id}"
-#     )
-
-# @app.post("/services/")
-# async def create_service(service_in: ServiceCreate):
-#     service_result = db.check_exits_service(service_in.name.lower())
-#     if service_result:
-#         return { "message": "Service already exits", "result": service_result }
-#     service_result = db.create_service(service_in.name.lower())
-#     return { "message": "Service added", "result": service_result }
-
-# @app.delete("/services/")
-# async def delete_service(service: str):
-#     db.delete_service(service)
-#     return { "message": "DONE" }
 
 app.include_router(router)
